815e010df235aabddfb51c417ded6f78b5d30681
lowlyphe <jones.kyle2893@gmail.com>
2022-08-04 18:36:17 -0400

    working on setting up the db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require("dotenv").config();
 const pg = require('pg');
 const { application } = require('express');
 
@@ -9,11 +10,29 @@ app.use(cors());
 
 const { DATABASE_URL, NODE_ENV, PORT } = process.env;
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
 const pool = new pg.Pool({
   connectionString: DATABASE_URL,
   ssl: NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
+app.get('/api/games', (req,res) => {
+  let first = getRandomInt(1, 296);
+  let second = getRandomInt(1, 296);
+  pool.query('SELECT * FROM games WHERE id in ($1, $2)', [first, second]).then((data) => {
+    res.set(200).type('applicaiton/json').send(data.rows)
+  })
+  
+})
+
+app.post('/api/games', (req,res) => {
+  pool.query('UPDATE')
+})
 
 
 app.listen(process.env.PORT || 3000, () => {
